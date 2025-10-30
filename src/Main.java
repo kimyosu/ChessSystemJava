6b57ab915341380627a38ba92ff88c94faecebc9
kimyosu <kimyosu@protonmail.com>
2025-10-29 23:16:41 -0300

    Correcting Object Name

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Position position = new Position(3, 5);
         System.out.println(position);
-        Board Board = new Board(8, 8);
+        Board board = new Board(8, 8);
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
     }
